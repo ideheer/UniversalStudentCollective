ff4395d521166c89234e647c825bb277b10f06ff
ideheer <ian.deheer@gmail.com>
2017-04-22 19:29:11 +0000

    Added homelist function for rendering provider list from mongodb

--- app_server/controllers/services.js
+++ app_server/controllers/services.js
@@ -31,7 +31,53 @@ module.exports.goods = function(req, res) {
 	res.render('goods', {title:'Goods'});
 };
 
-/* GET provider list */
+var renderHomepage = function(req, res){
+	res.render('provider-list', {
+		title: 'Universal Student Collective - Neato!',
+		sidebar: 'Find a student to help you out!',
+		providers: [{
+			name: 'Rick Sanchez',
+			address: '3122 Madera Ave, Oakland, CA 94619',
+			rating: '3',
+			attributes: ['Certified', 'Small Dogs', 'Medium Dogs'],
+			distance: '.2 mi'
+		},{
+			name: 'Gladys Bentley',
+			address: '2312 Teviot St. Los Angeles, CA, 93009',
+			rating: '5',
+			attributes: ['Certified', 'Cats', 'Piano'],
+			distance: '609'
+		},{
+			name: 'Samantha Bee',
+			address: '30 Rockefeller Plaza, New York, NY 10112',
+			rating: '4',
+			attributes: ['Small to Large Dogs', 'Cats', 'Sarcasm'],
+			distance: '2,907 mi'
+		}]
+	});
+};
+module.exports.homelist = function(req, res) {
+	var requestOptions, path;
+	path = '/api/providers';
+	requestOptions = {
+		url : apiOptions.server + path,
+		method : "GET",
+		json : {},
+		qs : {
+			lng : -112.393386,
+			lat : 48.671091,
+			maxDistance : 20
+		}
+	};
+	request(
+		requestOptions,
+		function(err, response, body) {
+			renderHomepage(req, res);
+		}
+	);
+};
+
+/* GET provider list 
 module.exports.providers = function(req, res) {
 	res.render('provider-list', {
 		title:'Service Providers',
@@ -56,4 +102,4 @@ module.exports.providers = function(req, res) {
 			distance: '2,907 mi'
 		}]
 	});
-};
+};*/
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// var ctrlMain = require('../controllers/main'); //3.4
 var ctrlServices = require('../controllers/services');
-// var ctrlOthers = require('../controllers/others');
 
 /* Locations pages */
 router.get('/', ctrlServices.login);
@@ -18,20 +16,7 @@ router.get('/rides', ctrlServices.rides);
 router.get('/goods', ctrlServices.goods);
 
 //get providers
-router.get('/providers', ctrlServices.providers);
+router.get('/providers', ctrlServices.homelist);
 
-//get events
-//router.get('/events', ctrlServices.events);
-
-//get myAccount
-//router.get('/myAccount', ctrlServices.myAccount);
-
-
-/* GET home page. */
-// var homepageController = function (req, res) {
-// 	res.render('index', {title: 'Express'});
-// }
-// router.get('/', homepageController);
-// router.get('/', ctrlMain.index);
  
 module.exports = router;
