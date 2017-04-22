a469de246239ad0432c406900ed5998e90ed0f28
ideheer <ian.deheer@gmail.com>
2017-04-22 19:01:34 +0000

    setting up request and NODE_ENV for live vs local

--- app_server/controllers/services.js
+++ app_server/controllers/services.js
@@ -1,4 +1,10 @@
 var request = require('request');
+var apiOptions = {
+	server : "https://getmean-ideheer.c9users.io:8080"
+};
+if (process.env.NODE_ENV === 'production') {
+	apiOptions.server = "https://usc.herokuapp.com";
+}
 
 /* GET 'home' page */
 module.exports.login = function(req, res) {
